0f2de3e4a3dedff5014cdf0d44c81f5be6582545
zhaoxiaotong01 <zhaoxiaotong01@gridsum.com>
2019-01-09 13:54:05 +0800

    initializr服务自己改造

---
+++ src/main/java/com/example/initialzrdemo/InitialzrDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.initialzrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialzrDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitialzrDemoApplication.class, args);
+    }
+
+}
+
